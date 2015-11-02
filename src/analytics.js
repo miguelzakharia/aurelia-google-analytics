80c81cb0bb5396c6500f153d175e00a40037b97c
Miguel Zakharia <miguel@zakharia.me>
2015-11-02 09:26:13 -0500

    WIP

--- src/analytics.js
+++ src/analytics.js
@@ -10,38 +10,88 @@ import {inject} from 'aurelia-dependency-injection';
 import {EventAggregator} from 'aurelia-event-aggregator';
 import * as LogManager from 'aurelia-logging';
 
+/*
+.plugin('aurelia-google-analytics', config => {
+			config.init('UA-69323125-1', 'ga');
+			config.attach({
+				logging: {
+					enabled: true
+				},
+				pageTracking: {
+					enabled: true
+				},
+				clickTracking: {
+					enabled: true,
+					filter: () => {
+						return element instanceof HTMLElement &&
+						(element.nodeName.toLowerCase() === 'a' ||
+							element.nodeName.toLowerCase() === 'button');
+					}
+				}
+			});
+		})
+*/
+
+const defaultOptions = {
+	enablePageTracking: false,
+	enableClickTracking: false,
+	enableLogging: false
+};
+
+const criteria = {
+	isElement: function(e) { return e instanceof HTMLElement; },
+	hasClass: function(cls) {
+		return function(e) {
+			return criteria.isElement(e) && e.classList.contains(cls);
+		}
+	},
+	isOfType: function(e, type) {
+		return criteria.isElement(e) && e.nodeName.toLowerCase() === type.toLowerCase();
+	},
+	isAnchor: function(e) {
+		return criteria.isOfType(e, 'a');
+	},
+	isButton: function(e) {
+		return criteria.isOfType(e, 'button');
+	}
+};
+
+const delegate = function(criteria, listener) {
+	return function(evt) {
+		let el = evt.target;
+		do {
+			if(!criteria(el)) continue;
+			evt.delegateTarget = el;
+			listener.apply(this, arguments);
+			return;
+		} while( (el = el.parentNode) );
+	};
+};
+
 @inject(EventAggregator)
 export class Analytics {
 	constructor(EventAggregator) {
-		this.eventAggregator = EventAggregator;
-		this.initialized = false;
-		this.logger = LogManager.getLogger('analytics');
-		this.shouldLog = false;
-		this.shouldTrack = false;
+		this._eventAggregator = EventAggregator;
+		this._initialized = false;
+		this._logger = LogManager.getLogger('analytics');
+		this._options = defaultOptions;
 	}
 
-	attach() {
-		if(!this.initialized) {
-			if(this.shouldLog) {
-				this.logger.error("Analytics must be initialized before use.");
-			}
-			throw new Error("Analytics must be initialized before use.");
+	attach(options = defaultOptions) {
+		this._options = Object.assign({}, defaultOptions, options);
+		const errorMessage = "Analytics must be initialized before use.";
+
+		if(!this._initialized) {
+			this._log('error', errorMessage);
+			throw new Error(errorMessage);
 		}
 
-		this.eventAggregator.subscribe('router:navigation:success',
-			payload => this.track(payload.instruction.fragment, payload.instruction.config.title));
-	}
-
-	enableLogging(value) {
-		this.shouldLog = value;
-	}
-
-	enableTracking(value) {
-		this.shouldTrack = value;
+		this._attachClickTracker();
+		this._attachPageTracker();	
 	}
 
 	init(id) {
-		var script = document.createElement('script');
+		const script = document.createElement('script');
 		script.text = "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){" +
 			"(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o)," +
 			"m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)" +
@@ -52,26 +102,55 @@ export class Analytics {
 		ga('create', id, 'auto');
 		ga('send', 'pageview');
 
-		this.initialized = true;
+		this._initialized = true;
 	}
 
 	isInitialized() {
-		return this.initialized;
+		return this._initialized;
 	}
 
-	track(path, title) {
+	_attachClickTracker() {
+		if(!this._options.enableClickTracking) { return; }
+
+
+	}
+
+	_attachPageTracker() {
+		if(!this._options.enablePageTracking) { return; }
+
+		this._eventAggregator.subscribe('router:navigation:success',
+			payload => this._trackPage(payload.instruction.fragment, payload.instruction.config.title));
+	}
+
+	_log(level, message) {
+		if(!this._options.enableLogging) { return; }
+
+		this._logger[level](message);
+	}
+
+	_trackClick(evt) {
+		if(!evt || !evt.delegateTarget || !criteria.hasTrackingInfo(evt.delegateTarget)) { return };
+
+		let element = evt.delegateTarget;
+		let tracking = {
+			category: element.getAttribute('data-analytics-category'),
+			action: element.getAttribute('data-analytics-action'),
+			label: element.getAttribute('data-analytics-label')
+		};
+
+		ga('send', 'event', tracking.category, tracking.action, tracking.label);
+	}
+
+	_trackPage(path, title) {
 		if(!this.shouldTrack) { return; }
-		if(!this.initialized) {
-			if(this.shouldLog) {
-				this.logger.warn("Try calling init() before calling 'track()'.");
-			}
+		if(!this._initialized) {
+			this._log('warn', "Try calling 'init()' before calling 'track()'.");
 			return;
 		}
 
-		if(this.shouldLog) { 
-			this.logger.debug(`track: path = '${path}', title = '${title}'`); 
-		}
 		ga('set', { page: path, title: title });
 		ga('send', 'pageview');
+
+		this._log('debug', `track: path = '${path}', title = '${title}'`); 
 	}
 }
